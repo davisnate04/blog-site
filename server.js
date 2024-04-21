06100d4351b74650a641ce3930b506b128a531ff
davisnate04 <davisnate04@gmail.com>
2024-04-20 23:50:24 -0400

    Updated the port

--- server.js
+++ server.js
@@ -35,5 +35,5 @@ app.use(express.static(path.join(__dirname, 'public')))
 app.use(routes);
 
 sequelize.sync({ force: false }).then(() => {
-    app.listen(PORT, () => console.log(`Now listening on localhost:${PORT}`));
+    app.listen(process.env.PORT || PORT, () => console.log(`Now listening on localhost:${PORT}`));
 });
