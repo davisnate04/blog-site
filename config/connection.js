117377b33e183b08bee460a9b5a377a8f68e0d16
davisnate04 <davisnate04@gmail.com>
2024-04-21 00:05:55 -0400

    Updated the port

--- config/connection.js
+++ config/connection.js
@@ -3,6 +3,9 @@ require('dotenv').config();
 
 let sequelize;
 
+if (process.env.JAWSDB_URL) {
+    sequelize = new Sequelize(process.env.JAWSDB_URL);
+} else {
 sequelize = new Sequelize(
     process.env.DB_NAME,
     process.env.DB_USER,
@@ -13,5 +16,5 @@ sequelize = new Sequelize(
         port: 3306
     }
 );
-
+}
 module.exports = sequelize;
